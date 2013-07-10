9b1e6ba16dcaee6ead389e6fedde6fa234e237fe
peter <peter.orsag@gmail.com>
2013-07-10 16:28:56 +0000

    web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var data = fs.readFileSync("index.html","utf8");
 console.log(data);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
