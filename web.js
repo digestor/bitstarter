1185fde74782722081f3118e024c1d468607cb5d
peter <peter.orsag@gmail.com>
2013-07-10 16:21:10 +0000

    now reads from file

--- web.js
+++ web.js
@@ -1,12 +1,4 @@
-var express = require('express');
+var fs = require("fs");
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var data = fs.readFileSync("index.html","utf8");
+console.log(data);
